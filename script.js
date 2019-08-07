31ffd431bb6365a419bbf9ebeb766ec71f7c3b2d
Haeju Jeong <haejujeo@usc.edu>
2019-08-07 00:09:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $('#product4').on('mouseover', () =>{
+    $('#presto1').animate({
+      height: '250%',
+      width: 'auto'
+    }, 30)
+    //$('#presto1').hide();
+  })
+});
